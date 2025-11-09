a4e3630be7d52c7b5575f71becc68f4cd0adfb73
JAINISH-CHAUHAN <jainish040405@gmail.com>
2025-11-09 16:11:08 +0530

    Initial Commit

---
+++ project/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from project.routers.post import router as post_router
+
+app = FastAPI()
+
+# app.include_router(post_router, prefix='/posts')
+
+app.include_router(post_router)
+
+
+
+
